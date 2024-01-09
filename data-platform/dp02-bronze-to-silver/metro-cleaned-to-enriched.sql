edadbce8f32349612edfad4ec211fb19448900c5
john-o-neville <john-o-neville>
2024-01-09 19:18:50 +0000

    Added cleaned to enriched query

---
+++ data-platform/dp02-bronze-to-silver/metro-cleaned-to-enriched.sql
@@ -0,0 +1,22 @@
+-- Databricks notebook source
+CREATE OR REFRESH LIVE TABLE metro_enriched
+(
+  tran_date DATE,
+  tran_details STRING,
+  tran_type STRING,
+  tran_amount DECIMAL(12,4),
+  balance DECIMAL(12,4)
+)
+COMMENT 'Enriches the data from metro_cleaned'
+AS
+SELECT
+  MC.tran_date,
+  MC.tran_details,
+  MC.tran_type,
+  CASE
+    WHEN MC.amount_in IS NOT NULL THEN MC.amount_in
+    ELSE MC.amount_out * -1
+  END AS tran_amount,
+  MC.balance
+FROM
+  live.metro_cleaned AS MC;
